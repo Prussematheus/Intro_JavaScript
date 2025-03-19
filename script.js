0442e0517551b7fca7c399640a9b285848af676f
prussematheus <matheus_f_prusse@estudante.sesisenai.org.br>
2025-03-19 10:11:57 -0300

    update

--- script.js
+++ script.js
@@ -26,3 +26,11 @@ let n1=parseFloat (prompt("Digite o primeiro número"));
 let n2=parseFloat (prompt("Digite o segundo número"));
 
 calcular (n1,n2);
+
+let numero = 10;
+let delay = 0;
+while (numero >=1){
+    console.log(numero);
+    numero--;
+
+}
