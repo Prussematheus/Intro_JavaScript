54fac11687e21baddff4fa604ccbf8053f8ee751
prussematheus <matheus_f_prusse@estudante.sesisenai.org.br>
2025-03-19 10:02:13 -0300

    update

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ function verificarPar(numero){
     }else{
         console.log('impar');
     }
-}
+};
 
 verificarPar(85)
 verificarPar(78)
+
+
+function calcular(n1,n2){
+const soma = n1 + n2;
+const subtracao = n1 - n2;
+const divisao = n2 !== 0 ? n1 / n2 'NÃO EXISTE DIVISÃO POR ZERO SEU ACÉFALO';
+const multiplicacao = n1 * n2;
+}
+
+let n1=parseFloat (prompt("Digite o primeiro número"));
+let n2=parseFloat (prompt("Digite o segundo número"));
+
+function calcular(n1,n2);
